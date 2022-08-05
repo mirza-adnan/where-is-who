53416cf49032361e8d286f59d0769b0a5f523833
mirza-adnan <mirza.adnan2205@gmail.com>
2022-08-05 19:12:16 +0600

    removed hours from timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,8 +5,8 @@ function Timer({ gameOngoing }) {
     const [time, setTime] = useState(0);
     // const milliseconds = ("0" + Math.floor((time / 10) % 100)).slice(-2);
     const seconds = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
-    const minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
-    const hours = ("0" + Math.floor(time / 3600000)).slice(-2);
+    const minutes = ("0" + Math.floor(time / 60000)).slice(-2);
+    // const hours = ("0" + Math.floor(time / 3600000)).slice(-2);
 
     useEffect(() => {
         let interval;
@@ -24,7 +24,7 @@ function Timer({ gameOngoing }) {
 
     return (
         <P>
-            {hours}:{minutes}:{seconds}
+            {minutes} : {seconds}
         </P>
     );
 }
