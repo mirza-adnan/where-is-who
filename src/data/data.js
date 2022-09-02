764c5ff544c045895b35c45819f736a6864ccb66
mirza-adnan <mirza.adnan2205@gmail.com>
2022-09-02 19:12:36 +0600

    added all character info

--- src/data/data.js
+++ src/data/data.js
@@ -11,12 +11,13 @@ import Waldo from "../assets/waldo.png";
 const levelsData = [
     {
         name: "The Loc Nar",
-        image: LocNar,
+        image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/the-loc-nar.jpg?alt=media&token=2f620521-aa82-42bc-b82f-712670ef3da9",
         id: uuid4(),
+        leaderboard: [],
         characters: [
             {
                 name: "Meg Griffin",
-                image: Meg,
+                image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/meg.png?alt=media&token=4e796475-3725-439f-98b4-0c65be65ceff",
                 found: false,
                 id: uuid4(),
                 minX: 435,
@@ -26,7 +27,7 @@ const levelsData = [
             },
             {
                 name: "Batman",
-                image: Batman,
+                image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/batman.png?alt=media&token=90eff230-ca71-4284-8342-feca0e18a01c",
                 found: false,
                 id: uuid4(),
                 minX: 93,
@@ -36,7 +37,7 @@ const levelsData = [
             },
             {
                 name: "Wilson",
-                image: Wilson,
+                image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/wilson.png?alt=media&token=7a0d65ef-5b67-4554-a264-03d56187c5d8",
                 found: false,
                 id: uuid4(),
                 minX: 827,
@@ -48,12 +49,13 @@ const levelsData = [
     },
     {
         name: "Universe 113",
-        image: Universe113,
+        image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/universe-113.jpg?alt=media&token=994a6927-fcee-4d3a-a79b-4ed5a9648bcc",
         id: uuid4(),
+        leaderboard: [],
         characters: [
             {
                 name: "Totoro",
-                image: Totoro,
+                image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/totoro.png?alt=media&token=594bd85d-2357-4f26-8da1-8275d240e56f",
                 found: false,
                 id: uuid4(),
                 minX: 693,
@@ -63,7 +65,7 @@ const levelsData = [
             },
             {
                 name: "Courage",
-                image: Courage,
+                image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/courage.png?alt=media&token=19d8e978-b785-4eb6-beb7-0c5937089bf8",
                 found: false,
                 id: uuid4(),
                 minX: 303,
@@ -73,7 +75,7 @@ const levelsData = [
             },
             {
                 name: "Waldo",
-                image: Waldo,
+                image: "https://firebasestorage.googleapis.com/v0/b/where-is-who-33b7c.appspot.com/o/waldo.png?alt=media&token=58aa7ed9-3a3b-4f88-bf6a-453ce6dcf376",
                 found: false,
                 id: uuid4(),
                 minX: 140,
